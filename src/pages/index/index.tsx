0705250aaed4ae1cd4be344edb90464d38054117
zhikangfan <zhikangfan@outlook.com>
2023-03-16 22:18:03 +0800

    init: 初始化项目

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,24 +1,31 @@
 import { Component, PropsWithChildren } from 'react'
 import { View, Text } from '@tarojs/components'
+import { AtButton } from 'taro-ui'
 import './index.scss'
 
-export default class Index extends Component<PropsWithChildren> {
+// export default class Index extends Component<PropsWithChildren> {
+//
+//   componentWillMount () { }
+//
+//   componentDidMount () { }
+//
+//   componentWillUnmount () { }
+//
+//   componentDidShow () { }
+//
+//   componentDidHide () { }
+//
+//   render () {
+//     return (
+//       <View className='index'>
+//         <Text>Hello world!</Text>
+//         <AtButton type='primary' size='normal'>按钮文案</AtButton>
+//       </View>
+//     )
+//   }
+// }
 
-  componentWillMount () { }
-
-  componentDidMount () { }
-
-  componentWillUnmount () { }
-
-  componentDidShow () { }
-
-  componentDidHide () { }
-
-  render () {
-    return (
-      <View className='index'>
-        <Text>Hello world!</Text>
-      </View>
-    )
-  }
+function Index() {
+  return <AtButton type='primary' size='normal'>按钮文案</AtButton>
 }
+export default Index;
